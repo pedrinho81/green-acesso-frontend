6e57e15f71cbfc6158b81a534edab7c2cb560d2e
Pedro Henrique <1pedrohenri@gmail.com>
2023-03-30 15:45:24 -0300

    fix: remove logs

--- context/api.tsx
+++ context/api.tsx
@@ -17,25 +17,20 @@ export const ApiProvider = ({ children }: any) => {
   const fetchData = async (id?: string) => {
     setIsLoading(true);
     try {
-      console.log(id)
       params = id ? id :
         `?page=${page}&name=${search}`
-      console.log(`${baseUrl}${params}`)
+      
       const res = await (await fetch(`${baseUrl}${params}`)).json()
-      console.log(!!res.error)
 
       res.error ? [toast.error(res.error), setCharacters(null)]
         : [setCharacters(res), setCharacterDetail(res)]
-      console.log(`**URL: ${baseUrl}${params}`)
-      console.log('@@fetch result:')
-      console.log(res)
+
     } catch (error) {
       toast.error('Ops, houve um erro inesperado.')
     }
     setIsLoading(false)
   }
   useEffect(() => {
-    console.log('%%ID' + id)
     fetchData(id)
   }, [search, page, id])
 
